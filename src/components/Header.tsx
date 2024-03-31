8f58665934aa558ad4c7e069cb24f4532334d26a
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-03-31 12:53:09 +0200

    add emoji to the name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-gray-900 p-2">
       <div className="flex items-center flex-shrink-0 tracking-tight text-white gap-1">
-        <i className="font-semibold text-xl">TimeShift</i>
+        <i className="font-semibold text-xl">Time⚡️Shift</i>
       </div>
     </nav>
   )
